81fac8ac1c9d2a2f9ce0d266bad169d6e1d9365b
Shawn Oakley <shawn.oakley@youvisit.com>
2019-01-07 11:00:11 -0500

    Adds comment

--- AspNetCore/Startup.cs
+++ AspNetCore/Startup.cs
@@ -52,7 +52,8 @@ namespace AspNetCore
                             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. 
+        // Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
